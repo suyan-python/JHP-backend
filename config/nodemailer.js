ae0300824a3c1d3ae6e98fd0e7381ce7eada10bf
Suyan Man Amatya <srkamatya007@gmail.com>
2025-09-25 16:22:16 +0545

    updated nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,11 +2,14 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-
+  port: 465,
+  secure: true, // must be true for 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // optional, helps with strict SSL
   },
 });
 
